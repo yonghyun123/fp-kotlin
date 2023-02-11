9cd1e79d351d9af65d8bafa7d2f712f78c955678
yonghyun123 <model9960@naver.com>
2023-02-11 16:35:08 +0900

    연습문제와 기본 연산

--- src/ex02/Practice02.kt
+++ src/ex02/Practice02.kt
@@ -1,3 +1,35 @@
 package ex02
 
+fun main() {
+    "Hello, ".product("kotlin");
+//    require("kotlin".addHelloPrefix() == "Hello, kotlin")
+//    require("FP".addHelloPrefix() == "Hello, FP")
+    print("$result")
+}
 
+fun String.product(value: String) {
+    println(this + value);
+}
+
+/**
+ * 답안지
+ * 불친절하네..
+ */
+
+private fun String.addHelloPrefix(): String = TODO();
+
+
+/**
+ * let을 이용한 객체 변경
+ */
+
+val person = Person("FP", 30)
+val result = person.let{
+    it.name = "Kotlin"
+    it.age = 10
+    it
+}
+
+data class Person(var name: String, var age: Int){
+
+}
