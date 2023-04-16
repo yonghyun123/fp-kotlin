1bbab936a60549c6d4cebb0f5fea7cc6b6cdd4bc
yonghyun123 <model9960@naver.com>
2023-04-16 14:46:28 +0900

    행위를 가진 타입 정의

--- src/ex06/practice06.kt
+++ src/ex06/practice06.kt
@@ -1,4 +1,58 @@
 package ex06
 
-class practice06 {
+import java.util.Objects
+
+fun main() {
+
+    val maybe1:Maybe<Int> = Just<Int>(5)
+    val maybe2 = Just(3)
+
+    println("value = ${maybe1} value2=${maybe2}")
+
+    val frontend = object : Frontend {}
+    val backend = object : Backend {}
+
+    frontend.writeCode()
+    backend.writeCode()
+
+    val fullStack = FullStack()
+
+    fullStack.writeCode()
+
+
+}
+
+sealed class Maybe<T>
+object Nothing: Maybe<kotlin.Nothing>()
+data class Just<T>(val value: T): Maybe<T>()
+
+interface Developer{
+    val language: String
+
+    fun writeCode(){
+        println("write $language")
+    }
+}
+
+interface Backend : Developer {
+    fun operateEnvironment(): String{
+        return "operateEnvironment"
+    }
+
+    override val language: String
+        get() = "Haskell"
+}
+
+interface Frontend : Developer {
+    fun drawUI(): String{
+        return "drawUI"
+    }
+
+    override val language: String
+        get() = "Elm"
+}
+
+class FullStack: Frontend, Backend{
+    override val language: String
+        get() = super<Backend>.language + super<Frontend>.language
 }
